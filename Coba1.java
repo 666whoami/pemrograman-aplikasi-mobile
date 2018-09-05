9ffa77f5d63fcad0f029eba4a015a8024a066e13
666whoami <37165186+666whoami@users.noreply.github.com>
2018-09-05 10:48:23 +0700

    Add files via upload

---
+++ Coba1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba1;
+
+/**
+ *
+ * @author WHOAMI
+ */
+public class Coba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int nilai=5;
+    for(int x=1;x<=nilai; x++){
+	for(int k=1;k<=x;k++){
+            System.out.print("*");
+	}
+	       System.out.println();
+    }
+
+    }
+    }
+    
